7b7726fa235bdd8b904804eb52431ed10b3e0b48
Gary Olson <golsonmke@gmail.com>
2020-04-15 09:17:40 -0500

    intit commit

---
+++ Go_Nosh/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Go_Nosh.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Go_Nosh.Areas.Identity.IdentityHostingStartup))]
+namespace Go_Nosh.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
